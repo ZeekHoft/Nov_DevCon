3e29253c2f1f2be3ab7370c44ca5743efc20ff3b
h1tmd <105845103+h1tmd@users.noreply.github.com>
2023-11-08 12:46:55 +0800

    Rework game theme

--- main.py
+++ main.py
@@ -8,30 +8,38 @@ import pyttsx3
 
 
 # Global variable
-whacks = 0
+points = 0
 game_timer = 20
 time_left = game_timer
+rating = {
+    0: "POORLY",
+    10: "BADLY",
+    20: "WELL!",
+    30: "GOOD",
+    40: "GREAT!",
+    50: "AWESOME!",
+    60: "WHACKED 'em ALL!",
+}
 
 # Constant variables
-HOLE_AMT = 4  # total holes is the square of this number
-HOLE_PADDING = 10
-BG_COLOR = "burlywood4"
-LIGHT_COLOR = "#faead6"
-DARK_COLOR = "#0c1703"
-OTHER_COLOR = "#eff3a0"
+GRID_AMT = 4  # total boxes is the square of this number
+GRID_PAD = 10
+BG_COLOR = "#f4f5f6"
+DARK_COLOR = "#2f3237"
+GREY_COLOR = "#d6d8db"
 
 
-# Start or reset the game
+# Start or restart the game
 def start(event):
-    global whacks, time_left
-    for i in range(len(hole)):
-        hole[i].config(state="disabled")
+    global points, time_left
+    # for i in range(len(targets)):
+    #     targets[i].config(state="disabled")
     time_left = game_timer
-    whacks = 0
+    points = 0
     timelabel.config(text=f"{time_left} seconds")
     remark.config(text="")
 
-    t = threading.Thread(target=whac_a_mole)
+    t = threading.Thread(target=gameproper)
     t.start()
 
 
@@ -61,62 +69,69 @@ def ready_set_whack():
     scorelabel.config(text="0 points")
 
 
-def change_state(hole_num):
-    hole[hole_num].config(image=holepng)
-    sleep(1)
-    hole[hole_num].config(state="normal", image=mole)
-    sleep(1)
-    hole[hole_num].config(
-        state="disabled", image=blank, command=partial(onwhack, hole_num)
-    )
+# Spawn a new entity in the target location
+def new_entity(index):
+    # Aswang
+    if random.getrandbits(1):
+        targets[index].config(
+            image=human, state="active", command=partial(onwhack, index, True)
+        )
+        sleep(1)
+        if not targets[index]["command"] == 0:
+            targets[index].config(image=aswang)
+
+    # Human
+    else:
+        targets[index].config(
+            image=human, state="active", command=partial(onwhack, index, False)
+        )
+
+    sleep(2)
+    targets[index].config(state="disabled", image=blank, command=0)
 
 
 # Game!
-def whac_a_mole():
+def gameproper():
     ready_set_whack()
 
     timer_thread = threading.Thread(target=time)
     timer_thread.start()
 
-    # Stop generating moles when the timer ends
-    last_number = None
+    # Stop generating aswangs when the timer ends
+    num_order = []
     while time_left:
-        hole_num = random.randint(0, HOLE_AMT**2 - 1)
-        if hole_num == last_number:
+        index = random.randint(0, GRID_AMT**2 - 1)
+        if index in num_order[-3:]:
             continue
-        last_number = hole_num
-        change_hole = threading.Thread(target=change_state, args=(hole_num,))
-        change_hole.start()
-        sleep(0.8)
+        num_order.append(index)
+        spawn_entity = threading.Thread(target=new_entity, args=(index,))
+        spawn_entity.start()
+        sleep(1.3)
 
-    change_hole.join()
-    timelabel.configure(text="Play Again")
+    spawn_entity.join()
 
     # Give remark
-    global whacks
-    rating = {
-        0: "POORLY",
-        10: "BADLY",
-        20: "WELL!",
-        30: "GOOD",
-        40: "GREAT!",
-        50: "AWESOME!",
-        60: "WHACKED 'em ALL!",
-    }
+    global points
     for minscore, rm in rating.items():
-        if whacks >= minscore and whacks < minscore + 10:
-            remark.config(text=f"You did\n{rm}", fg=LIGHT_COLOR)
+        if points >= minscore and points < minscore + 10:
+            remark.config(text=f"You did\n{rm}", fg=DARK_COLOR)
             engine.say(f"You did {rm}")
             engine.runAndWait()
             break
 
+    timelabel.configure(text="Play Again")
 
-# When the mole is hit
-def onwhack(num):
-    hole[num].config(command=0, relief="sunken", image=hitpng)
-    global whacks
-    whacks += 1
-    scorelabel.config(text=f"{whacks} points")
+
+# When the aswang is hit
+def onwhack(index, if_aswang):
+    global points
+    if if_aswang:
+        targets[index].config(command=0, relief="sunken", image=aswanghit)
+        points += 1
+    else:
+        targets[index].config(command=0, relief="sunken", image=humanhit)
+        points -= 1
+    scorelabel.config(text=f"{points} points")
 
 
 # Text to speech engine
@@ -133,23 +148,25 @@ screen.config(bg=BG_COLOR)
 
 
 # Title
-title = Label(text="Shoot'eM ole", font=("Stencil", 50), bg=BG_COLOR, fg=LIGHT_COLOR)
+title = Label(
+    text="Aswang Game (tentative)", font=("Chiller", 50), bg=BG_COLOR, fg=DARK_COLOR
+)
 title.grid(column=1, row=0, pady=10)
 screen.rowconfigure(0, weight=1)
 
 # Score
 scorelabel = Label(
-    text=whacks,
+    text=points,
     width=11,
     font=("Cascadia Code", 30),
     bg=DARK_COLOR,
-    fg=LIGHT_COLOR,
+    fg=BG_COLOR,
 )
 scorelabel.grid(column=1, row=1)
 screen.rowconfigure(1, weight=1)
 
 timelabel = Label(
-    text="Start", width=11, font=("Cascadia Code", 30), bg=LIGHT_COLOR, fg=DARK_COLOR
+    text="Start", width=11, font=("Cascadia Code", 30), bg=BG_COLOR, fg=DARK_COLOR
 )
 timelabel.bind(sequence="<Button-1>", func=start)
 timelabel.grid(column=1, row=2)
@@ -162,44 +179,44 @@ remark.place(relx=0.15, rely=0.5, anchor=CENTER)
 
 
 # Create playarea grid
-playarea = Frame(screen, bg=BG_COLOR)
+playarea = Frame(screen, bg=GREY_COLOR)
 playarea.grid(column=1, row=3)
 screen.columnconfigure(1, weight=1)
 screen.rowconfigure(3, weight=7)
 
 
 # Load all assets
-holepng = PhotoImage(file="hole.png").subsample(5)
-blank = PhotoImage(file="blank.png").subsample(5)
-mole = PhotoImage(file="mole.png").subsample(5)
-hitpng = PhotoImage(file="hit.png").subsample(5)
+blank = PhotoImage(file="./assets/blank.png").subsample(5)
+human = PhotoImage(file="./assets/human.png").subsample(5)
+humanhit = PhotoImage(file="./assets/humanhit.png").subsample(5)
+aswang = PhotoImage(file="./assets/aswang.png").subsample(5)
+aswanghit = PhotoImage(file="./assets/aswanghit.png").subsample(5)
 
 
-hole = []
-hole_index = 0
-for row in range(HOLE_AMT):
-    for col in range(HOLE_AMT):
+targets = []
+target_index = 0
+for row in range(GRID_AMT):
+    for col in range(GRID_AMT):
         target = Button(
             playarea,
             image=blank,
-            bg=BG_COLOR,
-            state="disabled",
-            activebackground=BG_COLOR,
+            bg=GREY_COLOR,
+            activebackground=GREY_COLOR,
             border=0,
-            command=partial(onwhack, hole_index),
+            command=0,
         )
-        hole.append(target)
-        hole[hole_index].grid(column=col, row=row, sticky=NSEW)
+        targets.append(target)
+        targets[target_index].grid(column=col, row=row, sticky=NSEW)
 
         playarea.columnconfigure(col, weight=1)
         playarea.rowconfigure(row, weight=1)
 
-        hole_index += 1
+        target_index += 1
 
 
-# Pad holes
+# Space out the grid
 for child in playarea.winfo_children():
-    child.grid_configure(padx=HOLE_PADDING, pady=HOLE_PADDING)
+    child.grid_configure(padx=GRID_PAD, pady=GRID_PAD)
 
 
 # Press q to end game
